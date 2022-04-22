81dfc5426eb35658a3148d9496dea43513fb362c
catalyst16812 <catalyst16812@gmail.com>
2022-04-23 04:41:05 +0530

    javarecap final

---
+++ JAVARECAP/javarecap.java
@@ -0,0 +1,193 @@
+package JAVARECAP;
+//import java.lang.ref.Cleaner;
+//import java.lang.reflect.Array;
+//import java.util.Arrays;
+//import java.util.Scanner;
+
+/*
+---------------------------------------------------------------------------------------------------------------
+        //Function
+---------------------------------------------------------------------------------------------------------------
+public class javarecap {
+
+    public static void print(int a,String b,String c)
+    {
+        System.out.println("My name is : "+b);
+        System.out.println("My age is : "+a);
+        System.out.println("My course is : "+c);
+    }
+    public static void main(String[] args) {
+        
+---------------------------------------------------------------------------------------------------------------
+        //Output
+
+        System.out.println("Hello World !");
+---------------------------------------------------------------------------------------------------------------        
+        
+         //Input
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your age :");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Input your name :");
+        String name = sc.nextLine();
+        System.out.println( "\nYour age is : "+age);
+        System.out.println("\nYour name is : "+name);
+---------------------------------------------------------------------------------------------------------------        
+
+        //primitive data types
+
+        byte age = 21;
+         int a = 201308;
+         float money = 101.2f;
+         String name1 = "Nikhil";
+         long mobile_num = 9540510177l;
+         char section = 'M';
+         boolean busy = true;
+         System.out.print("Name : "+name1+"\nAge : "+age+"\nMobile : "+mobile_num);
+         System.out.print("\nPincode : "+a+"\nMoney : "+money+"\nGender : "+section+"\nIs Available : "+busy);
+---------------------------------------------------------------------------------------------------------------        
+
+        //non primitive data types
+
+        //String
+
+        String name ="Nikhil ";
+        String surname = "Vishwakarma";
+        String fullname = name+surname;
+        System.out.println(name.length());
+        System.out.println("My name is "+fullname);
+        System.out.println(fullname.charAt(4));
+        String funny_name = fullname.replace("a", "p");
+        funny_name = fullname.replace("i", "o");
+        System.out.println("Your Funny Name is : "+ funny_name);
+---------------------------------------------------------------------------------------------------------------        
+
+        //Substring
+
+        String sentence ="Hello, My Name Is Nikhil Vishwakarma. I am Learning Java ";
+        System.out.println(sentence.substring(0, 5));
+---------------------------------------------------------------------------------------------------------------        
+
+        //Array
+---------------------------------------------------------------------------------------------------------------
+
+        //undefined initialization
+        
+        int[] marks = new int[15];
+        
+        //defined initialization
+        
+        int marks2[] = {10,20,30,40,50,60,70,80,90,100};
+        System.out.println(marks2[4]);
+        
+        //length
+        
+        System.out.println(marks.length);
+        
+        //sort
+        
+        Arrays.sort(marks2);
+        System.out.println(marks2[2]);
+---------------------------------------------------------------------------------------------------------------
+
+        //2D Arrays
+        
+        int[][] markstotal = {{10,20,30},{11,12,13},{22,23,24}};
+        System.out.println(markstotal[1][1]);
+---------------------------------------------------------------------------------------------------------------        
+
+        //casting
+        
+        int a =10;
+        double b =22.8;
+        double tot1 = b+a;
+        int tot2 = a+(int)b;
+        System.out.println("total float value : "+tot1+" \n total int value : "+tot2);
+---------------------------------------------------------------------------------------------------------------        
+
+        //constants
+
+        final float PI = 3.14f;
+---------------------------------------------------------------------------------------------------------------        
+       
+        //conditions
+        int a;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        if(a>10)
+            System.out.println("A is big than 10");
+        else if (a==10)
+            System.out.println("A is equal to 10");
+        else 
+            System.out.println("A is lower then 10");
+---------------------------------------------------------------------------------------------------------------        
+        //loops
+---------------------------------------------------------------------------------------------------------------        
+
+        //for loops
+        
+        for (int i =0;i<10;i++)
+         {
+             System.out.print(i+" ");
+         }
+
+        int i;
+        Scanner sc = new Scanner(System.in);
+---------------------------------------------------------------------------------------------------------------     
+        
+        //do while loops
+        
+        do{
+            System.out.print("input a number : ");
+            i = sc.nextInt();
+            System.out.println("\nYour number is : "+i);
+        } while (i!=0);
+---------------------------------------------------------------------------------------------------------------
+        
+        //while loops (break & continue)
+
+        int fun = 1;
+        System.out.flush();
+        while(true)
+        {
+            if (fun%2==0)
+            {
+                System.out.println("even");
+                fun++;
+                continue;
+            }
+            System.out.println(fun);
+                fun++;
+              if(fun>30)
+                break;    
+        }
+---------------------------------------------------------------------------------------------------------------         
+---------------------------------------------------------------------------------------------------------------
+
+        //call function print()
+
+         Scanner sc =new Scanner(System.in);
+         System.out.print("Input your name : ");
+         String name = sc.nextLine();
+         System.out.print("Input your age : ");
+         int age = sc.nextInt();
+         sc.nextLine();
+         System.out.print("Input your course : ");
+         String course = sc.nextLine();
+        --> print(age, name, course);
+
+
+    }
+---------------------------------------------------------------------------------------------------------------
+
+    //math module
+
+    public static void math(String[] args) {
+    //   System.out.println(Math.max(199102, 29102));
+    //   System.out.println((int)(Math.random()*100)); 
+    }
+}
+
+*/
